a31b15de4db435e972b112ca6916977bb8ff620a
cesarvelandia <cesar@velandia.co>
2015-04-12 21:19:05 +0800

    streamlining themes

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var minifycss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
 
 var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 
+// turn react's jsx files into js
 gulp.task('js', function(){
     browserify('./public/javascripts/src/app.jsx')
         .transform(reactify)
@@ -14,12 +18,22 @@ gulp.task('js', function(){
         .pipe(gulp.dest('public/javascripts/build/'));
 });
 
+var bootstrap = 'public/libraries/bootstrap-sass-official/stylesheets/bootstrap/_bootstrap.scss';
+var bootswatch = 'public/libraries/bootswatch-scss/flatly/_bootswatch.scss';
+
+// turn scss files into css
 gulp.task('sass', function() {
-    gulp.src('public/stylesheets/scss/*.scss')
+    gulp.src(['public/stylesheets/scss/*.scss', bootstrap, bootswatch])
         .pipe(sass())
+            .pipe(autoprefixer("last 3 version","safari 5", "ie 8", "ie 9"))
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('public/stylesheets/css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
         .pipe(gulp.dest('public/stylesheets/css'));
 });
 
+// look out for changes and recompile if needed
 gulp.task('watch', function() {
     gulp.watch("public/javascripts/src/**/*.jsx", ["js"])
     gulp.watch("public/stylesheets/scss/*.scss", ["sass"]);

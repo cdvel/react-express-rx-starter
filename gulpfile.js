0430e94c33f3ed59a52c76acf54f374a065fbfa4
cesarvelandia <cesar@velandia.co>
2015-06-05 13:27:36 +0800

    changes to paths

--- gulpfile.js
+++ gulpfile.js
@@ -18,12 +18,17 @@ gulp.task('js', function(){
         .pipe(gulp.dest('public/javascripts/build/'));
 });
 
-var bootstrap = 'public/libraries/bootstrap-sass-official/stylesheets/bootstrap/_bootstrap.scss';
-var bootswatch = 'public/libraries/bootswatch-scss/flatly/_bootswatch.scss';
+//var bootstrap = 'public/libraries/bootstrap-sass-official/stylesheets/bootstrap/_bootstrap.scss';
+//var bootswatch = 'public/libraries/bootswatch-scss/flatly/_bootswatch.scss';
+var vars = "public/stylesheets/scss/_variables.scss";
+var themevars = "../../libraries/bootswatch-scss/readable/_variables.scss";
+var bootstrap =  "../../libraries/bootstrap-sass-official/assets/stylesheets/_bootstrap.scss";
+var bootswatch =  "../../libraries/bootswatch-scss/readable/_bootswatch.scss";
+
 
 // turn scss files into css
 gulp.task('sass', function() {
-    gulp.src(['public/stylesheets/scss/*.scss', bootstrap, bootswatch])
+    gulp.src([vars1, themevars, bootstrap, bootswatch, 'public/stylesheets/scss/styles.scss'])
         .pipe(sass())
             .pipe(autoprefixer("last 3 version","safari 5", "ie 8", "ie 9"))
         .pipe(concat('styles.css'))
